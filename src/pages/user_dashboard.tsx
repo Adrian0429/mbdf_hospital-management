060ebfdc8b73826d7893d8a2b0a9f8b17b8be0ac
andrianang <andrianandrian503@gmail.com>
2023-06-09 14:34:21 +0700

    feat: user dashboard

--- src/pages/user_dashboard.tsx
+++ src/pages/user_dashboard.tsx
@@ -5,6 +5,12 @@ import {AiOutlineBook} from 'react-icons/ai'
 import {AiOutlineShoppingCart} from 'react-icons/ai'
 import {AiOutlineUser} from 'react-icons/ai'
 import {AiOutlinePoweroff} from 'react-icons/ai'
+import {AiOutlineSearch} from 'react-icons/ai'
+import {AiOutlineBell} from 'react-icons/ai'
+import {BsFillMoonFill} from 'react-icons/bs'
+import {AiOutlineInfoCircle} from 'react-icons/ai'
+import {BiUserCircle} from 'react-icons/bi'
+
 
 
 const user_dashboard = () => {
@@ -48,7 +54,24 @@ const user_dashboard = () => {
         <div className='absolute left-[435px] top-[68px]'>
            <h2 className='font-semibold text-xl'>Welcome, <span className='text-green-800'>Andrian</span></h2>
         </div>
-        <div className="absolute left-[1266px] top-[34px] border border-black rounded-lg w-60 h-14">
+        <div className="pl-4 absolute left-[1100px] top-[34px] rounded-full w-[422px] h-[61px] flex items-center">
+            <AiOutlineSearch className="absolute left-9 top-[32%] w-5 h-6 text-[#166C6D]"></AiOutlineSearch>
+            <input type="text" placeholder='Search' className='ml-3 pl-8 w-[214px] h-[41px] rounded-full bg-[#F4F7FE] border-none'/>
+            <AiOutlineBell className='ml-2 text-2xl text-gray-400'></AiOutlineBell>
+            <BsFillMoonFill className='ml-4 text-xl text-gray-400'></BsFillMoonFill>
+            <AiOutlineInfoCircle className='ml-3 text-2xl text-gray-400'></AiOutlineInfoCircle>
+            <BiUserCircle className='ml-3 text-4xl text-green-800 '></BiUserCircle>
+        </div>
+        <div className='border border-black w-[681px] h-[80.07px] absolute left-[421px] top-[200px] grid-cols-2'>
+            <div className='rounded-lg border border-black h-full w-1/4'>
+                <button className='pl-4 pb-3 w-full h-full flex items-center'>
+                        <AiOutlineBook className='mt-2 text-3xl text-green-800'></AiOutlineBook>
+                    <div className='flex-col'>
+                        <h2 className='ml-2 font-semibold'>Reservasi</h2>
+                        <p>Reservasi</p>
+                    </div>
+                </button>
+            </div>
 
         </div>
 
